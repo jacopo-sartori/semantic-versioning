5f43e04446fd5f8c85c0e947be770d90bb2defbf
jacopo-sartori <61713334+jacopo-sartori@users.noreply.github.com>
2020-07-04 22:09:37 +0100

    Add files via upload
    First commit

---
+++ semanticversioning.py
@@ -0,0 +1,142 @@
+import functools
+
+
+@functools.singledispatch
+def _parse(version):
+    """Return a dict representation of the version."""
+    if version is None:
+        return {"major": 0, "minor": 1, "patch": 0}
+    raise TypeError("Unsupported type")
+
+
+@_parse.register(str)
+def _from_str(version):
+    atoms = list(map(int, version.split(".")))
+    return _parse(atoms)
+
+
+@_parse.register(list)
+@_parse.register(tuple)
+def _from_iter(version):
+    if len(version) > 3:
+        raise ValueError("semver only supports major, minor, patch versions")
+    if len(version) == 0 or (len(version) == 1 and version[0] == 0):
+        return {"major": 0, "minor": 1, "patch": 0}
+    major = version[0]
+    try:
+        minor = version[1]
+    except IndexError:
+        minor = 0
+    try:
+        patch = version[2]
+    except IndexError:
+        patch = 0
+    return {"major": major, "minor": minor, "patch": patch}
+
+
+@_parse.register(int)
+def _from_int(version):
+    if version < 0:
+        raise ValueError("Only non-negative integers allowed")
+    return {"major": version, "minor": 0 if version else 1, "patch": 0}
+
+
+@_parse.register(dict)
+def _from_dict(version):
+    filtered = {k: v for k, v in version.items() if k in ("major", "minor", "patch")}
+    if "major" not in filtered:
+        filtered["major"] = 0
+    if "minor" not in filtered:
+        filtered["minor"] = 0 if filtered["major"] else 1
+    if "patch" not in filtered:
+        filtered["patch"] = 0
+    return filtered
+
+
+class Version:
+    def __init__(self, version=None):
+        for attr, value in _parse(version).items():
+            setattr(self, attr, value)
+
+    def __repr__(self):
+        return f"Version {self.major}.{self.minor}.{self.patch}"
+
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
+
+    @classmethod
+    def _convert(cls, obj):
+        """Convenience method to avoid lots of isinstance calls."""
+        if isinstance(obj, cls):
+            return obj
+        return cls(obj)
+
+    def _compare(self, other):
+        try:
+            other = self._convert(other)
+        except (TypeError, ValueError):
+            raise NotImplementedError()
+
+        if self.major > other.major:
+            return 1
+        if self.major < other.major:
+            return -1
+        if self.minor > other.minor:
+            return 1
+        if self.minor < other.minor:
+            return -1
+        if self.patch > other.patch:
+            return 1
+        if self.patch < other.patch:
+            return -1
+        return 0
+
+    def __lt__(self, other):
+        return self._compare(other) == -1
+
+    def __le__(self, other):
+        return self._compare(other) != 1
+
+    def __gt__(self, other):
+        return self._compare(other) == 1
+
+    def __ge__(self, other):
+        return self._compare(other) != -1
+
+    def __eq__(self, other):
+        return self._compare(other) == 0
+
+    def __ne__(self, other):
+        return self._compare(other) != 0
+
+    def bump_major(self):
+        return self.__class__({"major": self.major + 1, "minor": 0, "patch": 0})
+
+    def bump_minor(self):
+        return self.__class__(
+            {"major": self.major, "minor": self.minor + 1, "patch": 0}
+        )
+
+    def bump_patch(self):
+        return self.__class__(
+            {"major": self.major, "minor": self.minor, "patch": self.patch + 1}
+        )
+
+
+def compare(version_a, version_b):
+    version_a = Version._convert(version_a)
+    return version_a._compare(version_b)
+
+
+def max_version(versions):
+    def bigger(version_a, version_b):
+        return version_a if compare(version_a, version_b) >= 0 else version_b
+
+    return Version._convert(functools.reduce(bigger, versions))
+
+
+def min_version(versions):
+    def smaller(version_a, version_b):
+        return version_a if compare(version_a, version_b) < 0 else version_b
+
+    return Version._convert(functools.reduce(smaller, versions))
---
+++ test_semver.py
@@ -0,0 +1,160 @@
+import pytest
+
+import semanticversioning
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        ("1.2.3", {"major": 1, "minor": 2, "patch": 3}),
+        ("1.2", {"major": 1, "minor": 2, "patch": 0}),
+        ("1", {"major": 1, "minor": 0, "patch": 0}),
+    ],
+)
+def test_version_from_string(version, expected):
+    version = semanticversioning.Version(version)
+    assert version.major == expected["major"]
+    assert version.minor == expected["minor"]
+    assert version.patch == expected["patch"]
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        ([1, 2, 3], {"major": 1, "minor": 2, "patch": 3}),
+        ((1, 2), {"major": 1, "minor": 2, "patch": 0}),
+        ([1], {"major": 1, "minor": 0, "patch": 0}),
+        ((), {"major": 0, "minor": 1, "patch": 0}),
+    ],
+)
+def test_version_from_iterable(version, expected):
+    version = semanticversioning.Version(version)
+    assert version.major == expected["major"]
+    assert version.minor == expected["minor"]
+    assert version.patch == expected["patch"]
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        (1, {"major": 1, "minor": 0, "patch": 0}),
+        (0, {"major": 0, "minor": 1, "patch": 0}),
+    ],
+)
+def test_version_from_integer(version, expected):
+    version = semanticversioning.Version(version)
+    assert version.major == expected["major"]
+    assert version.minor == expected["minor"]
+    assert version.patch == expected["patch"]
+
+
+@pytest.mark.parametrize(
+    "version, expected",
+    [
+        ({"major": 1, "minor": 2, "patch": 3}, {"major": 1, "minor": 2, "patch": 3}),
+        (
+            {"major": 1, "patch": 3, "foo": 8, "bar": None},
+            {"major": 1, "minor": 0, "patch": 3},
+        ),
+        ({"minor": 2}, {"major": 0, "minor": 2, "patch": 0}),
+        ({}, {"major": 0, "minor": 1, "patch": 0}),
+    ],
+)
+def test_version_from_dictionary(version, expected):
+    version = semanticversioning.Version(version)
+    assert version.major == expected["major"]
+    assert version.minor == expected["minor"]
+    assert version.patch == expected["patch"]
+
+
+@pytest.mark.parametrize(
+    "version_a, version_b, expected",
+    [
+        (
+            "1.2.3",
+            "4.5",
+            {
+                "lt": True,
+                "lte": True,
+                "eq": False,
+                "neq": True,
+                "gt": False,
+                "gte": False,
+            },
+        ),
+        (
+            "1.2.3",
+            "1.2.3",
+            {
+                "lt": False,
+                "lte": True,
+                "eq": True,
+                "neq": False,
+                "gt": False,
+                "gte": True,
+            },
+        ),
+        (
+            "1.2",
+            "1",
+            {
+                "lt": False,
+                "lte": False,
+                "eq": False,
+                "neq": True,
+                "gt": True,
+                "gte": True,
+            },
+        ),
+    ],
+)
+def test_version_comparison(version_a, version_b, expected):
+    version_a = semanticversioning.Version(version_a)
+    assert (version_a < version_b) is expected["lt"]
+    assert (version_a <= version_b) is expected["lte"]
+    assert (version_a == version_b) is expected["eq"]
+    assert (version_a != version_b) is expected["neq"]
+    assert (version_a > version_b) is expected["gt"]
+    assert (version_a >= version_b) is expected["gte"]
+
+    version_b = semanticversioning.Version(version_b)
+    assert (version_a < version_b) is expected["lt"]
+    assert (version_a <= version_b) is expected["lte"]
+    assert (version_a == version_b) is expected["eq"]
+    assert (version_a != version_b) is expected["neq"]
+    assert (version_a > version_b) is expected["gt"]
+    assert (version_a >= version_b) is expected["gte"]
+
+
+def test_version_bump():
+    version = semanticversioning.Version("1.2.3")
+    assert version.bump_major() == "2.0.0"
+    assert version.bump_minor() == "1.3.0"
+    assert version.bump_patch() == "1.2.4"
+    assert version.bump_major().bump_minor().bump_patch() == "2.1.1"
+
+
+@pytest.mark.parametrize(
+    "version_a, version_b, expected",
+    [
+        ("1.2.3", "4.5", -1),
+        ("6", "6.0.0", 0),
+        ("3.6", "3", 1),
+        ("4.5", 3, 1),
+        ("0.1", [], 0),
+        ("3.3.7", (3, 3, 9), -1),
+        ("2", {"patch": 9}, 1),
+    ]
+)
+def test_compare(version_a, version_b, expected):
+    assert semanticversioning.compare(version_a, version_b) == expected
+
+
+def test_max_version():
+    versions = ["1.2", 7, (5, 4, 11), {"major": 3, "patch": 10}]
+    assert semanticversioning.max_version(versions) == "7.0.0"
+
+
+def test_min_version():
+    versions = ["1.2", 7, (5, 4, 11), {"major": 3, "patch": 10}]
+    assert semanticversioning.min_version(versions) == "1.2.0"
